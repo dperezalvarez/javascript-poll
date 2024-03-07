0810b56d1b730f1c4b36bfaa840178221963a676
dperezalvarez2 <dperezalvarez@viacoreit.com>
2024-03-07 04:10:55 -0800

    Javascript poll

---
+++ app.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const votes = {
+        JavaScript: 0,
+        Python: 0,
+        Ruby: 0,
+        Rust: 0,
+    };
+    //TODO-------
+
+});
